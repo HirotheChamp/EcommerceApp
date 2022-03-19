bc88e3d85e568a6ae7e2a436b04907fafe5ddeaf
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2022-03-19 16:48:21 -0700

    first commit

---
+++ Ecommerce_App/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import Main from './views/Main'
+import './App.css';
+
+function App() {
+  return (
+  <Main/>
+  );
+}
+
+export default App;
